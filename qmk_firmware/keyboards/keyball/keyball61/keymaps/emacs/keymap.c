a80f9f043d246cc09707742e180ad12a7857f2ff
applejxd <9639592+applejxd@users.noreply.github.com>
2025-01-30 22:24:27 +0900

    Emacsレイヤーのキー配置を修正し、レイアウトの整合性を向上

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/keymap.c
@@ -157,12 +157,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_LCTL  , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),    KC_BSPC,LT(2,KC_ENT)  , _______  ,_______   , _______  , KC_RALT  , KC_INT3
   ),
   // US 配列(印字)を JP キーボード(ソフト設定)として使用
-  [1] = LAYOUT_universal(,
+  [1] = LAYOUT_universal(
     S(KC_EQL), S(KC_1)   , KC_LBRC    , S(KC_3)  , S(KC_4)  , S(KC_5)  ,                                  KC_EQL   , S(KC_6)  ,S(KC_QUOT), S(KC_8)  , S(KC_9)   , S(KC_INT1),
     S(KC_TAB), S(KC_Q)   , S(KC_W)    , S(KC_E)  , S(KC_R)  , S(KC_T)  ,                                  S(KC_Y)  , S(KC_U)  , S(KC_I)  , S(KC_O)  , S(KC_P)   , S(KC_SCLN),
     S(KC_LCTL),S(KC_A)   , S(KC_S)    , S(KC_D)  , S(KC_F)  , S(KC_G)  ,                                  S(KC_H)  , S(KC_J)  , S(KC_K)  , S(KC_L)  , KC_QUOT   , S(KC_2)  ,
     _______  , S(KC_Z)   , S(KC_X)    , S(KC_C)  , S(KC_V)  , S(KC_B)  ,S(KC_RBRC),           S(KC_NUHS), S(KC_N)  , S(KC_M)  ,S(KC_COMM), S(KC_DOT), S(KC_SLSH), S(KC_RSFT),
-    S(KC_ESC), S(KC_LCTL), S(KC_LALT) , S(KC_LGUI), _______  , _______  , _______  ,            _______  , _______  , _______  ,S(KC_RGUI), _______ , S(KC_RALT), S(KC_INT3)
+    S(KC_ESC), S(KC_LCTL), S(KC_LALT) , S(KC_LGUI), _______  , _______ , _______  ,            _______  , _______ , _______  , _______  , _______  , S(KC_RALT), S(KC_INT3)
   ),
   // FN (TOP)・テンキー(左)・マウス(右)レイヤー。最左列はスクロール方向を変更。
   [2] = LAYOUT_universal(
