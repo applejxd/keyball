aae35470acf414321decc3a1c78ca2037e24bf2f
applejxd <9639592+applejxd@users.noreply.github.com>
2025-01-31 22:38:21 +0900

    DEL_TEXTキーの処理を修正し、キーの登録と解除を適切に行うように変更

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/keymap.c
@@ -75,8 +75,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case DEL_TEXT:
             if (record->event.pressed) {
-                tap_code16(KC_DEL);
                 set_mark_active = false;
+                register_code(KC_DEL);
+            } else {
+                unregister_code(KC_DEL);
             }
             break;
         case KC_LEFT: case KC_RIGHT: case KC_UP: case KC_DOWN: 
@@ -161,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , S(KC_MINS)   ,
     MO(4)    , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
     MO(1)    , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_RBRC  ,              KC_NUHS, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-    KC_ESC   , KC_LCTL  , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),    KC_BSPC,LT(2,KC_ENT)  , _______  ,_______   , _______  , KC_RALT  , KC_INT3
+    KC_ESC   , KC_LCTL  , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),       KC_BSPC,LT(2,KC_ENT),_______  ,_______   , _______  , KC_RALT  , KC_INT3
   ),
   // US 配列(印字)を JP キーボード(ソフト設定)として使用
   [1] = LAYOUT_universal(
