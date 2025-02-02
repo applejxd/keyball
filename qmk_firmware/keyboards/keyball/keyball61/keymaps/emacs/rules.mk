8064c682ff1fb6e19d30e7c9849e608d3c891a4f
applejxd <9639592+applejxd@users.noreply.github.com>
2025-02-03 02:42:35 +0900

    rules.mkでLTO_ENABLEの設定をコメントアウト

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/rules.mk
@@ -16,8 +16,8 @@ VIA_ENABLE = no
 
 # see https://zenn.dev/koron/articles/98324ab760e83a
 
-# Link Time Optimization
-LTO_ENABLE = yes
+# # Link Time Optimization
+# LTO_ENABLE = yes
 
 # # printf に必要なコンソール表示を無効化
 # CONSOLE_ENABLE = no
