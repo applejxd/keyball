7b5045dbf466c3322e6ca5df94dfd150cf4df180
applejxd <9639592+applejxd@users.noreply.github.com>
2025-02-03 01:55:35 +0900

    rules.mkでLTO_ENABLEを有効化し、CONSOLE_ENABLEの設定をコメントアウト

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/rules.mk
@@ -17,7 +17,7 @@ VIA_ENABLE = no
 # see https://zenn.dev/koron/articles/98324ab760e83a
 
 # Link Time Optimization
-# LTO_ENABLE = yes
+LTO_ENABLE = yes
 # printf に必要なコンソール表示を無効化
 # CONSOLE_ENABLE = no
 # Magic キーコードとほぼ重複する Command 機能を無効化
