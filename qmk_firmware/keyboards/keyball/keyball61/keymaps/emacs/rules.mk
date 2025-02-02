4408644a3f07fe516d7439b7b038e393eb8649d1
applejxd <9639592+applejxd@users.noreply.github.com>
2025-02-03 01:42:44 +0900

    rules.mkでのLTO_ENABLEとEXTRAKEY_ENABLEの定義をコメントアウト

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/rules.mk
@@ -17,13 +17,13 @@ RGBLIGHT_ENABLE = no
 # see https://zenn.dev/koron/articles/98324ab760e83a
 
 # Link Time Optimization
-LTO_ENABLE = yes
+# LTO_ENABLE = yes
 # printf に必要なコンソール表示を無効化
 CONSOLE_ENABLE = no
 # Magic キーコードとほぼ重複する Command 機能を無効化
 # COMMAND_ENABLE = no
 # メディアキーを無効化
-EXTRAKEY_ENABLE = no
+# EXTRAKEY_ENABLE = no
 # Shift または Ctrl キーで括弧を入力する機能を無効化
 SPACE_CADET_ENABLE = no
 # Shift キーまたは GUI キーで入力する Grave (~) を無効化
