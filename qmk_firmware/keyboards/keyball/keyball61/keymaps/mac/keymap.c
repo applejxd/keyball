634f217751b90449b03b152f0e04fa9e9453bff8
applejxd <9639592+applejxd@users.noreply.github.com>
2025-02-17 22:59:17 +0900

    Emacsレイヤーのキー配置を修正し、スペースキーとエンターキーの設定を追加

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/mac/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/mac/keymap.c
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     S(KC_TAB), S(KC_Q)   , S(KC_W)    , S(KC_E)  , S(KC_R)  , S(KC_T)  ,                                  S(KC_Y)  , S(KC_U)  , S(KC_I)  , S(KC_O)  , S(KC_P)   , S(KC_EQL),
     S(KC_LCTL),S(KC_A)   , S(KC_S)    , S(KC_D)  , S(KC_F)  , S(KC_G)  ,                                  S(KC_H)  , S(KC_J)  , S(KC_K)  , S(KC_L)  , S(KC_SCLN), S(KC_QUOT),
     _______  , S(KC_Z)   , S(KC_X)    , S(KC_C)  , S(KC_V)  , S(KC_B)  ,S(KC_LBRC),           S(KC_RBRC), S(KC_N)  , S(KC_M)  ,S(KC_COMM), S(KC_DOT), S(KC_SLSH), S(KC_RSFT),
-    S(KC_ESC), S(KC_LCTL), S(KC_LALT) , S(KC_LGUI), _______  , _______ , _______  ,            _______  , _______ , _______  , _______  , _______  , S(KC_RALT), S(KC_BSLS)
+    S(KC_ESC), S(KC_LCTL), S(KC_LALT) , S(KC_LGUI), _______ , S(KC_SPC), _______  ,           S(KC_BSPC), S(KC_ENT), _______  , _______  , _______  , S(KC_RALT), S(KC_BSLS)
   ),
   // FN (TOP)・テンキー(左)・マウス(右)レイヤー。最左列はスクロール方向を変更。
   [2] = LAYOUT_universal(
