2c2d80cba934908388c8fdf78aee1653b0b5e333
applejxd <9639592+applejxd@users.noreply.github.com>
2025-05-26 08:20:03 +0900

    レイヤー1のキー配置を修正し、KC_SCLNのレイヤーを変更。レイヤー2とレイヤー3のスクロールモードを入れ替え、コメントを更新。

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/keymap.c
@@ -144,7 +144,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
    S(KC_LBRC), KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , S(KC_MINS)  ,
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS,
-    MO(4)    , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , LT(3,KC_SCLN), S(KC_7)  ,
+    MO(4)    , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , LT(2,KC_SCLN), S(KC_7)  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_RBRC  ,              KC_NUHS, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT3  ,
     KC_ESC   , KC_LCTL  , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),       KC_BSPC,LT(2,KC_ENT),_______  ,_______   , _______  , KC_RALT  , MO(1)
   ),
@@ -198,13 +198,13 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     int layer_num = get_highest_layer(state);
 
     if (layer_num == 2) {
-        // レイヤー2では水平スクロール
-        keyball_set_scroll_mode(true);
-        keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_HORIZONTAL);
-    } else if (layer_num == 3) {
-        // レイヤー3では垂直スクロール
+        // レイヤー2では垂直スクロール
         keyball_set_scroll_mode(true);
         keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_VERTICAL);
+    } else if (layer_num == 3) {
+        // レイヤー3では水平スクロール
+        keyball_set_scroll_mode(true);
+        keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_HORIZONTAL);
     } else {
         // 他のレイヤーではスクロールを無効化
         keyball_set_scroll_mode(false);
