002428b7ef4ec75c4dc7b0d0cb3f9864a57abfe1
applejxd <9639592+applejxd@users.noreply.github.com>
2025-05-27 17:08:54 +0900

    レイヤー1のKC_SLSHをLT(2,KC_SLSH)に変更し、キー配置を調整しました。

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/emacs/keymap.c
@@ -145,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    S(KC_LBRC), KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , S(KC_MINS)  ,
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS,
     MO(4)    , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , LT(3,KC_SCLN), S(KC_7)  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_RBRC  ,              KC_NUHS, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT3  ,
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_RBRC  ,              KC_NUHS, KC_N     , KC_M     , KC_COMM  , KC_DOT   , LT(2,KC_SLSH), KC_INT3  ,
     KC_ESC   , KC_LCTL  , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),       KC_BSPC,LT(2,KC_ENT),_______  ,_______   , _______  , KC_RALT  , MO(1)
   ),
   // US 配列(印字)を JP キーボード(ソフト設定)として使用
