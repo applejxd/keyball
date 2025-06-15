d3905f21a340bd1664c21fd6237f37131f9b2fac
applejxd <9639592+applejxd@users.noreply.github.com>
2025-06-16 08:28:53 +0900

    アルファベットレイヤーのキー配置を修正し、LT(3,KC_A)とLSFT_T(KC_Z)を追加しました

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/39keys/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/39keys/keymap.c
@@ -143,11 +143,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // アルファベットレイヤー
   // 無効キーは右手トラックボールレイアウトでは存在しないキー
   [0] = LAYOUT_universal(
-    _______  , _______  , _______  , _______  , _______  , _______  ,                          _______  , _______  , _______  , _______  , _______      , _______  ,
-    _______  , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                          KC_Y     , KC_U     , KC_I     , KC_O     , KC_P         , _______  ,
-    _______  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                          KC_H     , KC_J     , KC_K     , KC_L     , LT(3,KC_SCLN), _______  ,
-    _______  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , _______  ,      _______, KC_N     , KC_M     , KC_COMM  , KC_DOT   , LT(1,KC_SLSH), _______  ,
-    _______  , KC_LCTL,KC_LALT,KC_LGUI,LSFT_T(KC_LNG2),LT(3,KC_SPC),LT(1,KC_LNG1),    KC_BSPC,LT(2,KC_ENT),_______ , _______  , _______  , KC_RSFT      , _______
+    _______  , _______       , _______  , _______  , _______  , _______  ,                          _______  , _______  , _______  , _______  , _______      , _______  ,
+    _______  , KC_Q          , KC_W     , KC_E     , KC_R     , KC_T     ,                          KC_Y     , KC_U     , KC_I     , KC_O     , KC_P         , _______  ,
+    _______  , LT(3,KC_A)    , KC_S     , KC_D     , KC_F     , KC_G     ,                          KC_H     , KC_J     , KC_K     , KC_L     , LT(3,KC_SCLN), _______  ,
+    _______  , LSFT_T(KC_Z)  , KC_X     , KC_C     , KC_V     , KC_B     , _______  ,      _______, KC_N     , KC_M     , KC_COMM  , KC_DOT   , LT(1,KC_SLSH), _______  ,
+    _______  , KC_LCTL       , KC_LALT,KC_LGUI,LSFT_T(KC_LNG2),LT(3,KC_SPC),LT(1,KC_LNG1), KC_BSPC,LT(2,KC_ENT),_______ , _______  , _______  , KC_RSFT      , _______
   ),
   // 記号専用レイヤー
   [1] = LAYOUT_universal(
