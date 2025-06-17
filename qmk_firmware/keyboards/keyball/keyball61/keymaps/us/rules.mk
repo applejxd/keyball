fd802c547530c8136fac279c27932d3691aacacf
applejxd <9639592+applejxd@users.noreply.github.com>
2025-06-18 08:07:59 +0900

    キー配置を修正し、US配列用のキーマップと設定ファイルを追加しました

---
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/us/rules.mk
@@ -0,0 +1,36 @@
+OLED_ENABLE = yes
+# LED 未使用でもマウス移動のために必要?
+RGBLIGHT_ENABLE = yes
+
+# ALT key キーの上書きのため
+# https://docs.qmk.fm/features/key_overrides
+KEY_OVERRIDE_ENABLE = yes
+
+# マウスキーに使用するコンボを有効化
+COMBO_ENABLE = yes
+
+#---------#
+# 容量節約 #
+#---------#
+
+VIA_ENABLE = no
+
+# 既に元ファイルで対策済み
+# see https://zenn.dev/koron/articles/98324ab760e83a
+# # Link Time Optimization
+# LTO_ENABLE = yes
+# # メディアキーを無効化
+# EXTRAKEY_ENABLE = no
+# # printf に必要なコンソール表示を無効化
+# CONSOLE_ENABLE = no
+# # Magic キーコードとほぼ重複する Command 機能を無効化
+# COMMAND_ENABLE = no
+# # Shift または Ctrl キーで括弧を入力する機能を無効化
+# SPACE_CADET_ENABLE = no
+# # Shift キーまたは GUI キーで入力する Grave (~) を無効化
+# GRAVE_ESC_ENABLE = no
+# # Magic キーコードを無効化
+# MAGIC_ENABLE = no
+
+# ミュージックモードを無効化
+MUSIC_ENABLE = no
