c49a0682baabd54f6469d40213232dafae8d4400
applejxd <9639592+applejxd@users.noreply.github.com>
2025-09-25 22:16:49 +0900

    fix(keymap): adjust keymap layouts for improved key placement and functionality

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/us/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/us/keymap.c
@@ -151,11 +151,11 @@ enum keymap_layer {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // 無効キーは右手トラックボールレイアウトでは存在しないキー
   [KL_BASE] = LAYOUT_universal(
-    KC_GRV   , KC_1        , KC_2     , KC_3   , KC_4           , KC_5             ,                                        KC_6               , KC_7    , KC_8     , KC_9     , KC_0              , KC_EQL  ,
-    KC_TAB   , KC_Q        , KC_W     , KC_E   , KC_R           , KC_T             ,                                        KC_Y               , KC_U    , KC_I     , KC_O     , LT(KL_EMACS,KC_P) , KC_MINS ,
-    MO(4)    , KC_A        , KC_S     , KC_D   , KC_F           , KC_G             ,                                        KC_H               , KC_J    , KC_K     , KC_L     , LT(KL_SMB,KC_SCLN), KC_QUOT ,
-    KC_LSFT  , LSFT_T(KC_Z), KC_X     , KC_C   , KC_V           , KC_B             , KC_LBRC             , KC_RBRC        , KC_N               , KC_M    , KC_COMM  , KC_DOT   , RSFT_T(KC_SLSH)   , KC_BSLS ,
-    KC_ESC   , KC_LCTL     , KC_LALT  , KC_LGUI, LSFT_T(KC_LNG2), LT(KL_SMB,KC_SPC), LT(KL_EMACS,KC_LNG1), LSFT_T(KC_BSPC), LT(KL_NUMFN,KC_ENT), _______ , _______  , _______  , KC_RSFT           , MO(KL_SFT)
+    KC_GRV   , KC_1        , KC_2     , KC_3   , KC_4           , KC_5             ,                                KC_6               , KC_7    , KC_8     , KC_9     , KC_0              , KC_EQL  ,
+    KC_TAB   , KC_Q        , KC_W     , KC_E   , KC_R           , KC_T             ,                                KC_Y               , KC_U    , KC_I     , KC_O     , LT(KL_EMACS,KC_P) , KC_MINS ,
+    MO(4)    , KC_A        , KC_S     , KC_D   , KC_F           , KC_G             ,                                KC_H               , KC_J    , KC_K     , KC_L     , LT(KL_SMB,KC_SCLN), KC_QUOT ,
+    KC_LSFT  , LSFT_T(KC_Z), KC_X     , KC_C   , KC_V           , KC_B             , KC_LBRC             , KC_RBRC, KC_N               , KC_M    , KC_COMM  , KC_DOT   , RSFT_T(KC_SLSH)   , KC_BSLS ,
+    KC_ESC   , KC_LCTL     , KC_LALT  , KC_LGUI, LSFT_T(KC_LNG2), LT(KL_SMB,KC_SPC), LT(KL_EMACS,KC_LNG1), KC_BSPC, LT(KL_NUMFN,KC_ENT), _______ , _______  , _______  , KC_RSFT           , MO(KL_SFT)
   ),
   // US 配列(印字)を JP キーボード(ソフト設定)として使用
   [KL_SFT] = LAYOUT_universal(
